50d8ae011d748ab3ec6b5fee27555864a9bc72f7
Vasiliy <w_av@mail.ru>
2023-12-01 22:17:35 +0300

    refactor

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-
+#!/home/szbaijie/usb_serial/env/bin/python3
 import sys
 
 import serial
 
+
 def work(dev):
     while True:
-        ser:serial.Serial
+        ser: serial.Serial
         with serial.Serial(dev, 115200, timeout=1) as ser:
             line = ser.read_until(expected=b'\xE2\x80\xA8')
             print(line.decode('utf-8'))
 
 
-
-
-
 if __name__ == '__main__':
-
     dev = sys.argv[1]
     work(dev)
